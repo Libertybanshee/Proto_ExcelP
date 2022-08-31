b6b4189ab552ea5093b4cf46245580ec9c76b14f
Libertybanshee <Libertybanshee1@gmail.com>
2022-08-31 15:10:31 +0200

    préparation

--- main.lua
+++ main.lua
@@ -6,7 +6,14 @@ end
 -- Cette ligne permet d'afficher des traces dans la console pendant l'éxécution
 io.stdout:setvbuf("no")
 
+local image = {}
+
+local frame = 1
+
 function love.load()
+    for n = 1, 17 do
+        image[n] = love.graphics.newImage("walk" .. n .. "png")
+    end
 end
 
 function love.update(dt)
